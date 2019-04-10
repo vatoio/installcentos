f40b92a8c438ad5eb6f3cf240ab8f980352ea37a
minhnguyenvato <46274222+minhnguyenvato@users.noreply.github.com>
2019-04-10 16:00:17 +0700

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -23,7 +23,6 @@ export IP_NODE_1=$(nextip $IP)
 export IP_NODE_2=$(nextip $IP_NODE_1)
 export IP_NODE_3=$(nextip $IP_NODE_2)
 export API_PORT=${API_PORT:="8443"}
-export LETSENCRYPT=${LETSENCRYPT:="false"}
 export MAIL=${MAIL:="developer@vato.vn"}
 
 ## Make the script interactive to set the variables
@@ -72,25 +71,6 @@ if [ "$INTERACTIVE" = "true" ]; then
 	if [ "$choice" != "" ] ; then
 		export API_PORT="$choice";
 	fi 
-
-	echo "Do you wish to enable HTTPS with Let's Encrypt?"
-	echo "Warnings: " 
-	echo "  Let's Encrypt only works if the IP is using publicly accessible IP and custom certificates."
-	echo "  This feature doesn't work with OpenShift CLI for now."
-	select yn in "Yes" "No"; do
-		case $yn in
-			Yes) export LETSENCRYPT=true; break;;
-			No) export LETSENCRYPT=false; break;;
-			*) echo "Please select Yes or No.";;
-		esac
-	done
-	
-	if [ "$LETSENCRYPT" = true ] ; then
-		read -rp "Email(required for Let's Encrypt): ($MAIL): " choice;
-		if [ "$choice" != "" ] ; then
-			export MAIL="$choice";
-		fi
-	fi
 	
 	echo
 
@@ -102,10 +82,6 @@ echo "* Your IP is $IP "
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
 echo "* OpenShift version: $VERSION "
-echo "* Enable HTTPS with Let's Encrypt: $LETSENCRYPT "
-if [ "$LETSENCRYPT" = true ] ; then
-	echo "* Your email is $MAIL "
-fi
 echo "******"
 
 # install updates
@@ -135,7 +111,7 @@ fi
 # install the packages for Ansible
 yum -y --enablerepo=epel install pyOpenSSL
 
-curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.7.8-1.el7.ans.noarch.rpm
 yum -y --enablerepo=epel install ansible.rpm
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-${VERSION} --depth=1
@@ -199,50 +175,6 @@ if [ ! -z "${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}" ]; then
 		__no_proxy="${IP},.${DOMAIN}"
 	fi
 	echo "openshift_no_proxy=\"${__no_proxy}\"" >> inventory.ini
-fi
-
-# Let's Encrypt setup
-if [ "$LETSENCRYPT" = true ] ; then
-	# Install CertBot
-	yum install --enablerepo=epel -y certbot
-
-	# Configure Let's Encrypt certificate
-	certbot certonly --manual \
-			--preferred-challenges dns \
-			--email $MAIL \
-			--server https://acme-v02.api.letsencrypt.org/directory \
-			--agree-tos \
-			-d $DOMAIN \
-			-d *.$DOMAIN \
-			-d *.apps.$DOMAIN
-	
-	## Modify inventory.ini 
-	# Declare usage of Custom Certificate
-	# Configure Custom Certificates for the Web Console or CLI => Doesn't Work for CLI
-	# Configure a Custom Master Host Certificate
-	# Configure a Custom Wildcard Certificate for the Default Router
-	# Configure a Custom Certificate for the Image Registry
-	## See here for more explanation: https://docs.okd.io/latest/install_config/certificate_customization.html
-	cat <<EOT >> inventory.ini
-	
-	openshift_master_overwrite_named_certificates=true
-	
-	openshift_master_cluster_hostname=console-internal.${DOMAIN}
-	openshift_master_cluster_public_hostname=console.${DOMAIN}
-	
-	openshift_master_named_certificates=[{"certfile": "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "cafile": "/etc/letsencrypt/live/${DOMAIN}/chain.pem", "names": ["console.${DOMAIN}"]}]
-	
-	openshift_hosted_router_certificate={"certfile": "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "cafile": "/etc/letsencrypt/live/${DOMAIN}/chain.pem"}
-	
-	openshift_hosted_registry_routehost=registry.apps.${DOMAIN}
-	openshift_hosted_registry_routecertificates={"certfile": "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem", "keyfile": "/etc/letsencrypt/live/${DOMAIN}/privkey.pem", "cafile": "/etc/letsencrypt/live/${DOMAIN}/chain.pem"}
-	openshift_hosted_registry_routetermination=reencrypt
-EOT
-	
-	# Add Cron Task to renew certificate
-	echo "@weekly  certbot renew --pre-hook=\"oc scale --replicas=0 dc router\" --post-hook=\"oc scale --replicas=1 dc router\"" > certbotcron
-	crontab certbotcron
-	rm certbotcron
 fi
 
 mkdir -p /etc/origin/master/
